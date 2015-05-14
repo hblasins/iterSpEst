3a0cc2362bf794ce83267a87d0c1adef4cb3caa8
Henryk Błasiński <hblasins@stanford.edu>
2015-05-13 22:23:17 -0700

    Fixed SNR simulations issues.

--- Figures/s_createFig3a.m
+++ Figures/s_createFig3a.m
@@ -10,9 +10,9 @@ close all;
 clear all;
 clc;
 
-fName = fullfile(iterSpEstRoot,'Results','noisyScene.mat');
+fName = fullfile(iterSpEstRoot,'Results','noisyLightScene.mat');
 if ~exist(fName,'file');
-    sName = fullfile(iterSpEstRoot,'s_analyzeNoisyScene.m');
+    sName = fullfile(iterSpEstRoot,'s_analyzeNoisyLightScene.m');
     run(sName);
 else
     load(fName);
@@ -22,13 +22,13 @@ fName = fullfile(iterSpEstRoot,'Data','sceneReference');
 load(fName);
 
 img = cat(3,sum(reflRefImg(:,:,41:end),3),sum(reflRefImg(:,:,21:40),3),sum(reflRefImg(:,:,1:20),3));
-img = img/max(img(:));
+img = (img/max(img(:))).^(1/2.2);
 
 %%
 
-lw = 1.5;
+lw = 1;
 fs = 6;
-ms = 5;
+ms = 3;
 f1 = figure;
 hold on; grid on; box on;
 plot(SNR,RMSELs,'g^-','lineWidth',lw,'markersize',ms);
@@ -38,8 +38,8 @@ plot(SNR,RMSEBoxSpatial,'co-','lineWidth',lw,'markersize',ms);
 xlabel('SNR, dB','fontsize',fs,'interpreter','latex');
 ylabel('RMSE','fontsize',fs,'interpreter','latex');
 ylim([0.4 1.4]);
-set(gca','XTick',12:4:36);
-set(gca','YTick',0.4:0.2:1.2);
+set(gca,'XTick',12:2:36);
+set(gca,'YTick',0.4:0.2:1.2);
 
 sz = get(gcf,'Paperposition');
 sz = 0.5*sz;
@@ -52,8 +52,10 @@ ax = axes('Position',[0.5 0.4 0.5 0.5]);
 imagesc(img);
 set(gca,'XTickLabel',[]);
 set(gca,'YTickLabel',[]);
+set(gca,'XTick',[]);
+set(gca,'YTick',[]);
 axis image;
 
 
-fName = fullfile(iterSpEstRoot,'Figures','noisyScene.eps');
+fName = fullfile(iterSpEstRoot,'Figures','noisyLightScene.eps');
 print('-depsc',fName);
